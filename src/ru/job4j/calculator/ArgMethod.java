c67312838e25689bc1f432315ae8b98b7a831cc3
Eduard1992hardcode <edchhe@yandex.ru>
2020-11-19 14:36:35 +0300

    4.1. Аргументы [#412791]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+    public static void hello(String name,int age) {
+        System.out.println("Hello, " + name);
+        System.out.println("Мой возраст: " + age +"лет");
+    }
+
+    public static void main(String[] args) {
+        String name = "Eduard Chelyadinov";
+        int age = 28;
+
+         ArgMethod.hello(name, age);
+    }
+}
