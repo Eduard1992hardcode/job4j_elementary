5b049520914b94288829d3e28eac529ad43353ff
Eduard1992hardcode <edchhe@yandex.ru>
2020-11-19 12:26:56 +0300

    3.3. Разбор первой программы. [#412800]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String [] args){
+        System.out.println("Chelyadinov Eduard");
+        System.out.println("10.09.1992");
+    }
+}
