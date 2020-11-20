6538334be764a928903cba8f5741101562eb3132
Eduard1992hardcode <edchhe@yandex.ru>
2020-11-20 14:30:49 +0300

    9.2. Checkstyle [#412796]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         System.out.println("Chelyadinov Eduard");
         System.out.println("10.09.1992");
     }
