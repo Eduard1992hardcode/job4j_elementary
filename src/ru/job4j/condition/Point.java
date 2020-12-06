2eb64a95fab5f9210bf70eea2d59b1ffe1bdd9b7
Eduard1992hardcode <edchhe@yandex.ru>
2020-12-06 18:56:38 +0300

    4. Расстояние между точками в трехмерном пространстве. [#412938]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -6,13 +6,22 @@ import static java.lang.StrictMath.sqrt;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public  Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
+    public  Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
     public double distance(Point another) {
         return sqrt(pow(another.x - this.x, 2) + pow(another.y - this.y, 2));
+    }
+    public double distance3d(Point another) {
+        return sqrt(pow(another.x - this.x, 2) + pow(another.y - this.y, 2) + pow(another.z - this.z, 2));
     }
     public static void main(String[] args) {
         Point a = new Point(0, 0);
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -10,44 +10,44 @@ public class PointTest {
 result (3, 7) to (6, 2) 1.0
 result (1, 1) to (10, 8) 16.0
 result (4, 15) to (6, 8) 6.0*/
-    @Test
+   @Test
     public void when00to20then2() {
         int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point first = new Point(0, 0);
+        Point second = new Point(2, 0);
+        double out = first.distance(second);
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
     public void when37to65then1() {
         int expected = 1;
-        int x1 = 3;
-        int y1 = 7;
-        int x2 = 6;
-        int y2 = 5;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point first = new Point(3, 7);
+        Point second = new Point(6, 5);
+        double out = first.distance(second);
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
     public void when11to108then16() {
         int expected = 16;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 10;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point first = new Point(1, 1);
+        Point second = new Point(10, 8);
+        double out = first.distance(second);
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
     public void when515to68then6() {
         int expected = 6;
-        int x1 = 5;
-        int y1 = 15;
-        int x2 = 6;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point first = new Point(5, 15);
+        Point second = new Point(6, 8);
+        double out = first.distance(second);
         Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void when5157to683then7() {
+        int expected = 7;
+        Point first = new Point(5, 15,7);
+        Point second = new Point(6, 8,3);
+        double out = first.distance(second);
+        Assert.assertEquals(expected, out, 0.1);
     }
 }
