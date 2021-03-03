6f8eef2144e55e3fd3f5f95e4c9f758cb4e72797
Eduard1992hardcode <edchhe@yandex.ru>
2021-03-03 17:55:04 +0300

    9. toString [#412968]

--- src/ru/job4j/tracker/StartUI.java
+++ src/ru/job4j/tracker/StartUI.java
@@ -4,6 +4,10 @@ public class StartUI {
     public static void main(String[] args) {
 Tracker trck = new Tracker();
         System.out.println(trck.findById(15));
+        Item item = new Item();
+        item.setId(55);
+        item.setName("first");
+        System.out.println(item);
     }
 }
 //Внутри метода main создайте объект Tracker. Вызовите у него метод add с объектом Item.
