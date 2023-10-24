3ae67f7e9fcd44d85942c4e3e5f500be580a42ae
Ed <Edchee@yandex.ru>
2023-10-24 15:27:41 +0300

    4.1. Разбор первой программы. [#159033 #148539]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        var multiplier = 1;
+        for (int i = 1; i < 10; i++) {
+            System.out.println("1 * " + multiplier +" = "+ multiplier);
+            multiplier++;
+        }
+    }
+
+}
