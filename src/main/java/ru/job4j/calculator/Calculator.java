a97caf32258c485bc86937ab32f69dc18a754a95
Ed <Edchee@yandex.ru>
2023-10-24 16:35:04 +0300

    4.1. Аргументы [#295521 #148531]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,15 +1,20 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / 2;
-        int fiveMinusTwo = five - 2;
-        int fourTimeTwo = four * 2;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+
+    public class ArgMethod {
+
+        public static void hello(String name, int age) {
+            System.out.println("Hello, " + name + ", age = " + age);
+        }
     }
-}
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+}4.1. Аргументы [#295521 #148531]
